7a7ce2c259efb708001632a77d3f2673e550b315
sbg224 <sambah450@gmail.com>
2025-01-13 15:35:17 +0100

    modification des données modif dates et heures

--- Datas.js
+++ Datas.js
@@ -1,6 +1,6 @@
 const datas = [
   {
-    id: 1,
+    id: 0,
     name: "Paris",
     description: "La ville lumière, célèbre pour sa Tour Eiffel et ses soirées inoubliables.",
     images: ["paris1.jpg", "paris2.jpg", "paris3.jpg", "paris4.jpg"],
@@ -12,10 +12,14 @@ const datas = [
       { model: "Mercedes Sprinter", capacity: 25, amenities: ["Sonorisation", "Mini-bar", "Wi-Fi"] },
       { model: "Volvo 9700", capacity: 50, amenities: ["Ecran géant", "Bar intégré", "Karaoké"] },
     ],
-    schedules: ["2025-01-15 22:00", "2025-01-20 22:00", "2025-01-25 22:00"],
+    schedules: [
+      { date: "2025-01-15", time: "22:00" },
+      { date: "2025-01-20", time: "23:00" },
+      { date: "2025-01-25", time: "00:10" },
+    ],
   },
   {
-    id: 2,
+    id: 1,
     name: "Marseille",
     description: "La cité phocéenne, réputée pour son vieux port et sa bouillabaisse.",
     images: ["marseille1.jpg", "marseille2.jpg", "marseille3.jpg", "marseille4.jpg"],
@@ -27,10 +31,14 @@ const datas = [
       { model: "Setra S416", capacity: 45, amenities: ["Climatisation", "Toilettes", "Wi-Fi"] },
       { model: "Iveco Daily", capacity: 20, amenities: ["Eclairage LED", "Prises USB", "Sonorisation"] },
     ],
-    schedules: ["2025-01-15 22:00", "2025-01-18 22:00", "2025-01-21 22:00"],
+    schedules: [
+      { date: "2025-01-15", time: "22:00" },
+      { date: "2025-01-20", time: "23:00" },
+      { date: "2025-01-25", time: "01:00" },
+    ],
   },
   {
-    id: 3,
+    id: 2,
     name: "Lyon",
     description: "La capitale de la gastronomie française, située entre deux fleuves.",
     images: ["lyon1.jpg", "lyon2.jpg", "lyon3.jpg", "lyon4.jpg"],
@@ -42,10 +50,14 @@ const datas = [
       { model: "MAN Lion's Coach", capacity: 40, amenities: ["Bar", "Ecrans HD", "Wi-Fi"] },
       { model: "Scania Touring", capacity: 55, amenities: ["Toilettes", "Espace bagages", "Sonorisation"] },
     ],
-    schedules: ["2025-01-16 22:00", "2025-01-19 22:00", "2025-01-22 22:00"],
+    schedules: [
+      { date: "2025-01-15", time: "21:50" },
+      { date: "2025-01-20", time: "23:10" },
+      { date: "2025-01-25", time: "00:30" },
+    ],
   },
   {
-    id: 4,
+    id: 3,
     name: "Toulouse",
     description: "La ville rose, connue pour son industrie aéronautique et ses charmantes rues.",
     images: ["toulouse1.jpg", "toulouse2.jpg", "toulouse3.jpg", "toulouse4.jpg"],
@@ -57,10 +69,14 @@ const datas = [
       { model: "Renault Tracer", capacity: 30, amenities: ["Climatisation", "Ecrans", "Wi-Fi"] },
       { model: "Volvo B8R", capacity: 45, amenities: ["Toilettes", "Prises USB", "Sonorisation"] },
     ],
-    schedules: ["2025-01-17 22:00", "2025-01-22 22:00", "2025-01-27 22:00"],
+    schedules: [
+      { date: "2025-01-15", time: "22:45" },
+      { date: "2025-01-20", time: "01:00" },
+      { date: "2025-01-25", time: "00:10" },
+    ],
   },
   {
-    id: 5,
+    id: 4,
     name: "Nice",
     description: "Ville méditerranéenne célèbre pour sa promenade des Anglais et ses plages.",
     images: ["nice1.jpg", "nice2.jpg", "nice3.jpg", "nice4.jpg"],
@@ -72,10 +88,14 @@ const datas = [
       { model: "Irizar i6", capacity: 40, amenities: ["Wi-Fi", "Climatisation", "Bar"] },
       { model: "Van Hool EX", capacity: 55, amenities: ["Toilettes", "Sonorisation", "Prises USB"] },
     ],
-    schedules: ["2025-01-16 22:00", "2025-01-23 22:00", "2025-01-30 22:00"],
+    schedules: [
+      { date: "2025-01-15", time: "23:00" },
+      { date: "2025-01-20", time: "23:30" },
+      { date: "2025-01-25", time: "00:10" },
+    ],
   },
   {
-    id: 6,
+    id: 5,
     name: "Nantes",
     description: "Une ville dynamique avec des machines géantes et une riche culture.",
     images: ["nantes1.jpg", "nantes2.jpg", "nantes3.jpg", "nantes4.jpg"],
@@ -87,10 +107,14 @@ const datas = [
       { model: "Mercedes Intouro", capacity: 50, amenities: ["Wi-Fi", "Climatisation", "Bar"] },
       { model: "Isuzu Novo", capacity: 25, amenities: ["Sonorisation", "Ecran LED", "USB"] },
     ],
-    schedules: ["2025-01-18 22:00", "2025-01-25 22:00", "2025-01-31 22:00"],
+    schedules: [
+      { date: "2025-01-15", time: "22:00" },
+      { date: "2025-01-20", time: "23:00" },
+      { date: "2025-01-25", time: "00:10" },
+    ],
   },
   {
-    id: 7,
+    id: 6,
     name: "Strasbourg",
     description: "Une ville européenne connue pour ses canaux et son marché de Noël.",
     images: ["strasbourg1.jpg", "strasbourg2.jpg", "strasbourg3.jpg", "strasbourg4.jpg"],
@@ -102,10 +126,14 @@ const datas = [
       { model: "Temsa Safari", capacity: 45, amenities: ["Toilettes", "Wi-Fi", "Prises USB"] },
       { model: "MAN Lions", capacity: 55, amenities: ["Climatisation", "Bar", "Sonorisation"] },
     ],
-    schedules: ["2025-01-19 22:00", "2025-01-24 22:00", "2025-01-28 22:00"],
+    schedules: [
+      { date: "2025-01-15", time: "22:45" },
+      { date: "2025-01-20", time: "01:00" },
+      { date: "2025-01-25", time: "00:10" },
+    ],
   },
   {
-    id: 8,
+    id: 7,
     name: "Lille",
     description: "Une ville chaleureuse avec une vie nocturne animée et une riche histoire.",
     images: ["lille1.jpg", "lille2.jpg", "lille3.jpg", "lille4.jpg"],
@@ -117,10 +145,14 @@ const datas = [
       { model: "Neoplan Cityliner", capacity: 40, amenities: ["Ecran LED", "Bar", "Wi-Fi"] },
       { model: "Setra Comfort", capacity: 50, amenities: ["Sonorisation", "USB", "Climatisation"] },
     ],
-    schedules: ["2025-01-20 22:00", "2025-01-26 22:00", "2025-02-01 22:00"],
+    schedules: [
+      { date: "2025-01-15", time: "23:45" },
+      { date: "2025-01-20", time: "01:00" },
+      { date: "2025-01-25", time: "00:10" },
+    ],
   },
   {
-    id: 9,
+    id: 8,
     name: "Bordeaux",
     description: "La capitale mondiale du vin, bordée par la Garonne.",
     images: ["bordeaux1.jpg", "bordeaux2.jpg", "bordeaux3.jpg", "bordeaux4.jpg"],
@@ -132,10 +164,14 @@ const datas = [
       { model: "Volvo 9900", capacity: 50, amenities: ["Wi-Fi", "Toilettes", "Bar"] },
       { model: "Iveco Magelys", capacity: 55, amenities: ["Ecran HD", "Climatisation", "Prises USB"] },
     ],
-    schedules: ["2025-01-18 22:00", "2025-01-24 22:00", "2025-01-29 22:00"],
+    schedules: [
+      { date: "2025-01-15", time: "22:45" },
+      { date: "2025-01-20", time: "01:00" },
+      { date: "2025-01-25", time: "00:10" },
+    ],
   },
   {
-    id: 10,
+    id: 9,
     name: "Montpellier",
     description: "Une ville ensoleillée, dynamique et proche de la mer.",
     images: ["montpellier1.jpg", "montpellier2.jpg", "montpellier3.jpg", "montpellier4.jpg"],
@@ -147,7 +183,11 @@ const datas = [
       { model: "Mercedes Travego", capacity: 50, amenities: ["Bar", "Wi-Fi", "Toilettes"] },
       { model: "Van Hool TX", capacity: 55, amenities: ["USB", "Ecran HD", "Climatisation"] },
     ],
-    schedules: ["2025-01-16 22:00", "2025-01-22 22:00", "2025-01-30 22:00"],
+    schedules: [
+      { date: "2025-01-15", time: "22:45" },
+      { date: "2025-01-20", time: "01:00" },
+      { date: "2025-01-25", time: "00:30" },
+    ],
   },
 ];
 
